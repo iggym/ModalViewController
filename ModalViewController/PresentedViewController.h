56fdede63f5af0647d6bf82b06612c98a1d37b5a
iggym <iggy.mw@gmail.com>
2013-07-08 13:08:44 -0500

    added presented scene, delegate, buttons and labels

---
+++ ModalViewController/PresentedViewController.h
@@ -0,0 +1,13 @@
+//
+//  PresentedViewController.h
+//  ModalViewController
+//
+//  Created by Iggy Mwangi on 7/8/13.
+//  Copyright (c) 2013 Iggy Mwangi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PresentedViewController : UIViewController
+
+@end
---
+++ ModalViewController/PresentedViewController.m
@@ -0,0 +1,38 @@
+//
+//  PresentedViewController.m
+//  ModalViewController
+//
+//  Created by Iggy Mwangi on 7/8/13.
+//  Copyright (c) 2013 Iggy Mwangi. All rights reserved.
+//
+
+#import "PresentedViewController.h"
+
+@interface PresentedViewController ()
+
+@end
+
+@implementation PresentedViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
