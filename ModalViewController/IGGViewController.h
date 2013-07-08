8c396810d04e6683eda5e8f6123a19f22f5dc553
iggym <iggy.mw@gmail.com>
2013-07-08 12:41:25 -0500

    Initial Commit

---
+++ ModalViewController/IGGViewController.h
@@ -0,0 +1,13 @@
+//
+//  IGGViewController.h
+//  ModalViewController
+//
+//  Created by Iggy Mwangi on 7/8/13.
+//  Copyright (c) 2013 Iggy Mwangi. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface IGGViewController : UIViewController
+
+@end
